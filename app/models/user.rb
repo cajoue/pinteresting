cb8f820fb84d398b6da487e2db1daffe4b21bd8b
cajoue <cajoue@users.noreply.github.com>
2016-09-06 19:23:45 +0200

    feat: add format validator to password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :password, :presence => true,
+                       :on => :create,
+                       :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ ,
+                                    message: "requires at least one of each of the following: number, lowercase letter, uppercase letter and special character" }
+
 end

f4a2940145e1e6f1d216becd28b4263b2167b2c4
cajoue <cajoue@users.noreply.github.com>
2016-09-08 12:12:52 +0200

    chore: add pins/user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
                        :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ ,
                                     message: "requires at least one of each of the following: number, lowercase letter, uppercase letter and special character" }
 
+  has_many :pins
+
 end

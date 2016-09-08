b8492e4e3b3e39ade4ac1b117bc3ef070fbb2299
cajoue <cajoue@users.noreply.github.com>
2016-09-08 20:44:44 +0200

    fix: remove format validator doesn't seem to work in heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :password, :presence => true,
-                       :on => :create,
-                       :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ ,
-                                    message: "requires at least one of each of the following: number, lowercase letter, uppercase letter and special character" }
-
   has_many :pins
 
 end

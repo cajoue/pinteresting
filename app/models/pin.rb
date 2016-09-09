f4f8d98c899717aff82244b20882544c43946586
cajoue <cajoue@users.noreply.github.com>
2016-09-09 21:10:38 +0200

    feat: add user and pin validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ class Pin < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
